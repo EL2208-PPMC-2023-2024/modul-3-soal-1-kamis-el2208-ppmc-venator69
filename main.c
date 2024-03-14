64f9bc418a6cdf2d52d66ff57d09b4f9fcebad23
venator69 <116265823+venator69@users.noreply.github.com>
2024-03-14 10:45:20 +0700

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
  *   Nama File           : main.c 
  *   Deskripsi           : decoder
  */
-
 #include <stdio.h>
 #include <string.h>
 int main(){
@@ -14,22 +13,27 @@ long int code, arrayofinput[3][5],len = 0,i,j;
 FILE *test;
 test = fopen("test.txt","r");
 char full[3][100];
+//menghapus row dan menjadikan array
 for (i=0;i<3;i++){
     fgets(full[i],100,test);
     full[i][strcspn(full[i],"\n")] = 0;
 }
+//menjadikan substring pada row sebagai masing-masing array
 
 char token[3][5][100];
 for (i=0;i<3;i++){
+    char *ptr = strtok(full[i]," ");
     for (j=0;j<5;j++){
-    token[i][j] = strtok(full[i]," ");
+        token[i][j] = strcpy(*ptr, token[i][j]);
+        ptr = strtok(NULL," ");
+    }  
     }
-}
 
+//input dari key 
 printf("masukkan key: ");
 scanf("%ld",&code);
 
-
+//menjadikan key sebagai array
 for (j =0;j<3;j++){
     for (i = 0; i<5 ; i++){
         int temp = code % 10;
